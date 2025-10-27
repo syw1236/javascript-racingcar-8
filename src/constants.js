ce8ac8c0816bbf87118da80869354f3caca9806b
syw1236 <syw1236@naver.com>
2025-10-27 19:13:44 +0900

    feat(constants): 문자열은 상수로 따로 빼내어 구현

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const ERROR_MEESAGE = {
+    emptyName: "[ERROR] 이름은 비어 있을 수 없습니다.",
+    overNameSize: "[ERROR] 자동차 이름은 5자 이하만 가능합니다.",
+    existSameName: "[ERROR] 중복된 이름이 존재합니다.",
+    tryCount: "[ERROR] 시도 횟수는 1 이상의 숫자여야 합니다.",
+};
+
+export const EXPLAIN = {
+    carName: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+    tryCount: "시도할 횟수는 몇 회인가요?",
+    result: "\n실행 결과",
+    winner: "최종 우승자 : ",
+}
