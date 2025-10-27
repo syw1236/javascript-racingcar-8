f8b23fe54cd46afdf93a92be85890bd5a2cfd530
syw1236 <syw1236@naver.com>
2025-10-27 19:14:07 +0900

    feat(App): 자동차 경주 실행되도록 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Console } from "@woowacourse/mission-utils";
+import { EXPLAIN } from "./constants.js";
+import InputValidator from "./utils/InputValidator.js";
+import RacingGame from "./core/RacingGame.js";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+    Console.print(EXPLAIN.carName);
+    const input = await Console.readLineAsync();
+    const carNames = InputValidator.validateCarName(input);
+
+    Console.print(EXPLAIN.tryCount);
+    const tryInput = await Console.readLineAsync();
+    const tryCount = InputValidator.validateTryCount(tryInput);
+
+    const game = new RacingGame(carNames, tryCount);
+    await game.start();
+
+    } catch(error) {
+      Console.print(error.message);
+      throw error;
+    }
+  }
 }
 
 export default App;
